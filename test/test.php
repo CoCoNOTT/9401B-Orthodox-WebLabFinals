a30b88e69f192b9eb0dff644c78f771b5a23e8a8
2161997 <35597192+2161997@users.noreply.github.com>
2019-05-03 12:24:41 +0800

    test file for php

---
+++ test/test.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT user_id, firsname, lastname FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["user_id"]. " - Name: " . $row["firsname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+
+
+
+</body>
+</html>
